6ec3c30a781d9b32de6bbf32e302fb78f540fb6b
Keiran <keiranonline@gmail.com>
2020-10-05 00:06:18 +0100

    Base site created, requires: layout, colors, content (devices, services)

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// Animate smooth scroll
+$('#view-services').on('click', function(){
+    const choosedevice = $('#choose-device').position().top;
+
+   $('html, body').animate({
+       scrollTop: choosedevice
+   }, 900); 
+});
+
+$('#view-phone').on('click', function(){
+    const phone = $('#phone').position().top;
+
+   $('html, body').animate({
+       scrollTop: phone
+   }, 900); 
+});
+
+$('#view-pc').on('click', function(){
+    const pc = $('#pc').position().top;
+
+   $('html, body').animate({
+       scrollTop: pc
+   }, 900); 
+});
+
+$('#view-console').on('click', function(){
+    const console = $('#console').position().top;
+
+   $('html, body').animate({
+       scrollTop: console
+   }, 900); 
+});
