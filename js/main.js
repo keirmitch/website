9148a2229a88d204a8628c6a79fba57a5a843b44
Keiran <keiranonline@gmail.com>
2020-11-15 14:01:31 +0000

    Added logo, colour scheme, updated content

--- js/main.js
+++ js/main.js
@@ -30,3 +30,19 @@ $('#view-console').on('click', function(){
        scrollTop: console
    }, 900); 
 });
+
+$('#view-iphone').on('click', function(){
+    const iphone = $('#iphone').position().top;
+
+   $('html, body').animate({
+       scrollTop: iphone
+   }, 900); 
+});
+
+$('#view-android').on('click', function(){
+    const android = $('#android').position().top;
+
+   $('html, body').animate({
+       scrollTop: android
+   }, 900); 
+});
