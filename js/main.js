16bb969a2624cada7abe01b8de1a5eeb24b21912
Keiran <keiranonline@gmail.com>
2020-12-08 13:10:44 +0000

    Source code tidy, do over

--- js/main.js
+++ js/main.js
@@ -1,48 +1,11 @@
-// Animate smooth scroll
-$('#view-services').on('click', function(){
-    const choosedevice = $('#choose-device').position().top;
-
-   $('html, body').animate({
-       scrollTop: choosedevice
-   }, 900); 
-});
-
-$('#view-phone').on('click', function(){
-    const phone = $('#phone').position().top;
-
-   $('html, body').animate({
-       scrollTop: phone
-   }, 900); 
-});
-
-$('#view-pc').on('click', function(){
-    const pc = $('#pc').position().top;
-
-   $('html, body').animate({
-       scrollTop: pc
-   }, 900); 
-});
-
-$('#view-console').on('click', function(){
-    const console = $('#console').position().top;
-
-   $('html, body').animate({
-       scrollTop: console
-   }, 900); 
-});
-
-$('#view-iphone').on('click', function(){
-    const iphone = $('#iphone').position().top;
-
-   $('html, body').animate({
-       scrollTop: iphone
-   }, 900); 
-});
-
-$('#view-android').on('click', function(){
-    const android = $('#android').position().top;
-
-   $('html, body').animate({
-       scrollTop: android
-   }, 900); 
-});
+// Animate Smooth Scroll
+$('#view-services').on('click', function() {
+    const services = $('#services').position().top;
+  
+    $('html, body').animate(
+      {
+        scrollTop: services
+      },
+      900
+    );
+  });
